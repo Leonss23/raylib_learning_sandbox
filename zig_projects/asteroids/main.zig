7698a0850293926396b492922f90513f1cb6d468
Leonardo Gatti [Leonss23] <leonardogattidev@gmail.com>
2023-08-09 06:25:14 -0300

    refactored `fpsCounter` and `getBuffer`

--- zig_projects/asteroids/main.zig
+++ zig_projects/asteroids/main.zig
@@ -16,26 +16,33 @@ pub fn main() !void {
     const refresh_rate = rl.GetMonitorRefreshRate(current_monitor);
     rl.SetTargetFPS(refresh_rate);
 
-    var buf: [10]u8 = undefined;
-
     while (!rl.WindowShouldClose()) {
+        print("buf: {any}\n", .{buffer});
         rl.BeginDrawing();
         defer rl.EndDrawing();
+        rl.ClearBackground(rl.RAYWHITE);
         //
 
-        buf = .{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        try fpsCounter(getBuffer());
 
-        rl.ClearBackground(rl.RAYWHITE);
-
-        const fps: c_int = rl.GetFPS();
-
-        const fps_c_str: [*c]const u8 = try cint_to_cstr(fps, &buf);
-
-        rl.DrawText(fps_c_str, 10, 10, 24, rl.BLACK);
-        print("\n", .{});
+        // rl.DrawRec
     }
+}
+
+var buffer: [50]u8 = undefined;
+fn getBuffer() []u8 {
+    @memset(&buffer, 0);
+    return &buffer;
 }
 
 fn cint_to_cstr(cint: c_int, buf: []u8) ![*c]const u8 {
     return @ptrCast(try std.fmt.bufPrint(buf, "{}", .{cint}));
+}
+
+fn fpsCounter(buf: []u8) !void {
+    const fps = rl.GetFPS();
+
+    const fps_c_str: [*c]const u8 = try cint_to_cstr(fps, buf);
+
+    rl.DrawText(fps_c_str, 10, 10, 24, rl.BLACK);
 }
