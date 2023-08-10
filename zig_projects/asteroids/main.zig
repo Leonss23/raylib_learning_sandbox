8216b246d9e0f7a37a3535a2ebe4c8cc8c0ce8bb
Leonardo Gatti [Leonss23] <leonardogattidev@gmail.com>
2023-08-10 09:43:33 -0300

    refactored some `main` code

--- zig_projects/asteroids/main.zig
+++ zig_projects/asteroids/main.zig
@@ -1,45 +1,71 @@
+pub const ray = @cImport({
+    @cInclude("raylib.h");
+});
 const std = @import("std");
 const print = std.debug.print;
 const utils = @import("utils");
-const rl = @cImport({
-    @cInclude("raylib.h");
-});
+const Asteroid = @import("asteroid.zig").Asteroid;
 
-pub fn main() !void {
+const globals = struct {
     const screen_width = 1920;
     const screen_height = 1080;
     const window_title = "Asteroids";
-    rl.SetConfigFlags(rl.FLAG_WINDOW_RESIZABLE);
-    rl.InitWindow(screen_width, screen_height, window_title);
-    defer rl.CloseWindow();
+    var buffer: [8]u8 = undefined;
+};
 
-    const current_monitor = rl.GetCurrentMonitor();
-    const refresh_rate = rl.GetMonitorRefreshRate(current_monitor);
-    rl.SetTargetFPS(refresh_rate);
+pub fn main() !void {
+    ray.SetConfigFlags(ray.FLAG_WINDOW_RESIZABLE);
+    ray.InitWindow(globals.screen_width, globals.screen_height, globals.window_title);
+    defer ray.CloseWindow();
+    //
+    startup();
 
-    while (!rl.WindowShouldClose()) {
-        print("buf: {any}\n", .{buffer});
-        rl.BeginDrawing();
-        defer rl.EndDrawing();
-        rl.ClearBackground(rl.RAYWHITE);
+    while (!ray.WindowShouldClose()) {
+        ray.BeginDrawing();
+        defer ray.EndDrawing();
         //
-
-        try fpsCounter(getBuffer());
-
-        // rl.DrawRec
+        handleInput();
+        try update();
     }
 }
 
-var buffer: [50]u8 = undefined;
+fn startup() void {
+    limitFPS();
+    initSpawn();
+}
+
+fn handleInput() void {}
+
+fn update() !void {
+    ray.ClearBackground(ray.RAYWHITE);
+    try fpsCounter(getBuffer());
+    print("buf: {any}\n", .{globals.buffer});
+}
+
+fn initSpawn() void {
+    const ast1 = Asteroid.newAsteroid(
+        ray.Vector2{ .x = 200.0, .y = 200.0 },
+        ray.Vector2{ .x = 0.0, .y = 0.0 },
+    );
+
+    print("asteroid1: {any}\n", .{ast1});
+}
+
+fn limitFPS() void {
+    const current_monitor = ray.GetCurrentMonitor();
+    const refresh_rate = ray.GetMonitorRefreshRate(current_monitor);
+    ray.SetTargetFPS(refresh_rate);
+}
+
 fn getBuffer() []u8 {
-    @memset(&buffer, 0);
-    return &buffer;
+    @memset(&globals.buffer, 0);
+    return &globals.buffer;
 }
 
 fn fpsCounter(buf: []u8) !void {
-    const fps = rl.GetFPS();
+    const fps = ray.GetFPS();
 
     const fps_c_str: [*c]const u8 = try utils.cint_to_cstr(fps, buf);
 
-    rl.DrawText(fps_c_str, 10, 10, 24, rl.BLACK);
+    ray.DrawText(fps_c_str, 10, 10, 24, ray.BLACK);
 }
